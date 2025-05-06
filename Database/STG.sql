1a395e7dcfd73259a2424da3c26dc290eb34c6d4
Mennahassann <147339385+Mennahassann@users.noreply.github.com>
2025-05-07 01:49:03 +0300

    Update STG.sql

--- Database/STG.sql
+++ Database/STG.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE CoffeeShop_STG;
+create DATABASE CoffeeShop_STG;
 
 USE CoffeeShop_STG;
 
@@ -8,7 +8,9 @@ CREATE TABLE stg_staff (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     position VARCHAR(20),
-    sal_per_hour DECIMAL(10, 2)
+    sal_per_hour DECIMAL(10, 2),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 CREATE TABLE stg_items (
@@ -17,7 +19,9 @@ CREATE TABLE stg_items (
     item_name VARCHAR(50),
     item_cat VARCHAR(20),
     item_size VARCHAR(10),
-    item_price DECIMAL(10, 2)
+    item_price DECIMAL(10, 2),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 CREATE TABLE stg_orders (
@@ -27,7 +31,9 @@ CREATE TABLE stg_orders (
     item_id VARCHAR(10),
     quantity INT,
     cust_name VARCHAR(50),
-    in_or_out VARCHAR(10)
+    in_or_out VARCHAR(10),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 
@@ -39,20 +45,26 @@ CREATE TABLE stg_recipes (
     ing_weight DECIMAL(10, 2),
     ing_meas VARCHAR(10),
     ing_price DECIMAL(10, 2),
-	recipe_quantity DECIMAL(10, 2)
+	recipe_quantity DECIMAL(10, 2),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 CREATE TABLE stg_inventory (
     inv_id VARCHAR(10),
     ing_id VARCHAR(10),
-    quantity DECIMAL(10, 2)
+    quantity DECIMAL(10, 2),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 CREATE TABLE stg_shift (
     shift_id VARCHAR(10),
     day_of_week VARCHAR(10),
     start_time TIME,
-    end_time TIME
+    end_time TIME,
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
 
 
@@ -61,5 +73,23 @@ CREATE TABLE stg_rota (
     rota_id VARCHAR(10),
     date DATE,
     shift_id VARCHAR(10),
-    staff_id VARCHAR(10)
+    staff_id VARCHAR(10),
+	src_update_date  datetime,
+  create_timestamp	datetime
 );
+
+CREATE TABLE Conf_Table
+(
+  table_name		 varchar(30),
+  last_extract_date	 datetime
+);
+
+INSERT INTO Conf_Table VALUES
+	('orders', '1950-01-01'),
+	('items', '1950-01-01'),
+	('inventory', '1950-01-01'),
+	('rota', '1950-01-01'),
+	('shift', '1950-01-01'),
+	('staff', '1950-01-01'),
+	('ingredients', '1950-01-01'),
+	('recipes', '1950-01-01');
